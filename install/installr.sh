732b7dc9e5fc93ae9eb25de38e9d3c7641130001
Ben Mason <ben@allpurposeben.info>
2018-10-04 14:44:28 -0400

    Added FB stuff and code
    Added the items from the FB intra and added initial code to use it.

--- install/installr.sh
+++ install/installr.sh
@@ -13,6 +13,9 @@
 # for it, eliminating the headaches of trying to install things from the gimped Recovery environment. 
 # Other Changes include baking an OS install app (of your choosing) into the DMG so we can upgrade as 
 # part of the (optional) nuke and pave step.
+#
+# Also adding some arguments to allow for both setting munki settings and host/computer name at installr time
+# and detecting existing settings and re-using them.
 
 
 if [[ $EUID != 0 ]] ; then
@@ -28,7 +31,7 @@ IFS=$'\n'
 # so we get to use Bash pattern matching
 BASENAME=${0##*/}
 THISDIR=${0%$BASENAME}
-PACKAGESDIR="${THISDIR}packages"
+PACKAGESDIR="${THISDIR}payload"
 INSTALLMACOSAPP=$(echo "${THISDIR}Install macOS"*.app)
 STARTOSINSTALL=$(echo "${THISDIR}Install macOS"*.app/Contents/Resources/startosinstall)
 
@@ -36,6 +39,8 @@ if [ ! -e "$STARTOSINSTALL" ]; then
     echo "Can't find an Install macOS app containing startosinstall in this script's directory!"
     exit -1
 fi
+
+## TODO validate options for munki and host/computer name, either passed or picked
 
 echo "****** Welcome to installr! ******"
 echo "macOS will be installed from:"
@@ -45,22 +50,30 @@ for PKG in $(/bin/ls -1 "${PACKAGESDIR}"/*.pkg); do
     echo "    ${PKG}"
 done
 echo
-echo "Available volumes:"
-for VOL in $(/bin/ls -1 /Volumes) ; do
-    if [[ "${VOL}" != "OS X Base System" ]] ; then
-        let INDEX=${INDEX}+1
-        VOLUMES[${INDEX}]=${VOL}
-        echo "    ${INDEX}  ${VOL}"
-    fi
-done
-read -p "Install to volume # (1-${INDEX}): " SELECTEDINDEX
+## TODO, add echo of detected hostname/munki settings
 
-SELECTEDVOLUME=${VOLUMES[${SELECTEDINDEX}]}
-
-if [[ "${SELECTEDVOLUME}" == "" ]]; then
-    exit 0
+# Detect target drive options.
+# Will default to existing volume named "Macintosh HD" unless interrupted or not pressent 
+if [[ -d "/Volumes/Macintosh HD" ]]; then
+	SELECTEDVOLUME="/Volumes/Macintosh HD"
+else
+	# No Macintosh HD, lets ask
+	for VOL in $(/bin/ls -1 /Volumes) ; do
+		if [[ "${VOL}" != "OS X Base System" ]] ; then
+			let INDEX=${INDEX}+1
+			VOLUMES[${INDEX}]=${VOL}
+			echo "    ${INDEX}  ${VOL}"
+		fi
+	done
+	read -p "Install to volume # (1-${INDEX}): " SELECTEDINDEX
+	SELECTEDVOLUME=${VOLUMES[${SELECTEDINDEX}]}
+	# sanity check
+	if [[ "${SELECTEDVOLUME}" == "" ]]; then
+		exit 0
+	fi
 fi
 
+# TODO, make moot if argument specifies to do so
 read -p "Erase target volume before install (y/N)? " ERASETARGET
 
 case ${ERASETARGET:0:1} in
@@ -68,12 +81,21 @@ case ${ERASETARGET:0:1} in
     * ) echo ;;
 esac
 
+# Prepare the first boot infrastructure 
+
+# Copy the infrastructure files to the new disk
+
+# Enumerate the items in the FBpayload folder into the fb_installers folder on disk, in numbered folders
+
+
 echo
 echo "Installing macOS to /Volumes/${SELECTEDVOLUME}..."
 
 # build our startosinstall command
 CMD="\"${STARTOSINSTALL}\" --agreetolicense --volume \"/Volumes/${SELECTEDVOLUME}\"" 
 
+
+### Remove below, keep reference
 for ITEM in "${PACKAGESDIR}"/* ; do
     FILENAME="${ITEM##*/}"
     EXTENSION="${FILENAME##*.}"
